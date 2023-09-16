132485ce16505facc0d5cec2d0d0da27ca369af0
Jeesun Bari <jeesunbari2002@gmail.com>
2023-09-16 22:51:30 +0530

    Add two files on list and list comprehention

---
+++ list-comprehension.py
@@ -0,0 +1,3 @@
+fruits = ["apple", "banana", "kiwi", "cherry", "mango"]
+newList = [x for x in fruits if "a" in x]
+print(newList)
---
+++ listt.py
@@ -0,0 +1,12 @@
+marks = [3, 4, 5]
+marks.append(5)
+marks.append(True)
+marks.append("Jeesun")
+marks.append("6")
+print(marks)
+print(type(marks))
+print(marks[-2])
+
+print(marks[1:4])
+print(marks[1:5+1])
+print(marks[1:5:2])
